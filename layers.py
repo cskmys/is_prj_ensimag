7a9f358e2f17a6dde7e8aab9359fd4392445b808
chakkak <karthik-subramanyam.chakka@grenoble-inp.org>
2020-04-30 20:52:48 +0200

    doesnt work but committing just in case

--- layers.py
+++ layers.py
@@ -1,18 +1,30 @@
 from keras.layers.core import Dense, Dropout
+from keras.layers import Conv2D, MaxPooling2D, Flatten, AveragePooling2D
 
 import global_const as gc
 
 
-def get_layer_param_str(layer):
-    if hasattr(layer, 'rate'):
-        return str(layer.rate)
-    if hasattr(layer, 'activation'):
-        return layer.activation.__name__
-    return ''
-
-
 def get_layer_name(layer):
     return layer.__class__.__name__
+
+
+def get_layer_param_str(layer):
+    layer_name = get_layer_name(layer)
+    if layer_name == 'Dropout':
+        return str(layer.rate)
+    if layer_name == 'Dense':
+        return layer.activation.__name__
+    if layer_name == 'Conv2D':
+        (x, y) = layer.kernel_size
+        return str('{fil}f{x}x{y}').format(fil=layer.filters, x=x, y=y)
+    if layer_name == 'MaxPooling2D':
+        (x, y) = layer.pool_size
+        return str('{x}x{y}').format(x=x, y=y)
+    if layer_name == 'Flatten':
+        return ''
+    if layer_name == 'AveragePooling2D':
+        return ''
+    raise TypeError('Layer ' + layer.__class__.__name__ + ' still unsupported')
 
 
 def set_name_for_layer(layers, layer_idx):
@@ -41,3 +53,26 @@ def DropOut(rate):
 
 def OpDense():
     return Dense(units=gc.prj.data.nb_class, activation='softmax')
+
+
+def ConvIp(filter, kernel_shape):
+    img_rows = gc.prj.data.actual_ip.train_ip.shape[1]
+    img_cols = gc.prj.data.actual_ip.train_ip.shape[2]
+    ip_shape = (img_rows, img_cols, 1)
+    return Conv2D(filters=filter, kernel_size=kernel_shape, input_shape=ip_shape, activation=gc.prj.model.activation)
+
+
+def Conv(filter, kernel_shape):
+    return Conv2D(filters=filter, kernel_size=kernel_shape, activation=gc.prj.model.activation)
+
+
+def MaxPooling(pool_shape):
+    return MaxPooling2D(pool_size=pool_shape)
+
+
+def Flat():
+    return Flatten()
+
+
+def AvgPooling():
+    return AveragePooling2D()
--- main.py
+++ main.py
@@ -7,10 +7,10 @@ import layers as l
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Supress warning and informational messages
 
-for i in range(5):
-    gc.init_test_data(lr=0.01, optimizer='sgd', loss_func='categorical_crossentropy', activation='relu', callback=None, nb_epochs=2, batch_siz=8 * 1024)
-    dat.proc_data()
-    gc.prj.model.layers = [l.IpDense(512), l.DropOut((i+1)/20), l.Dense(512), l.Dropout(0.2), l.OpDense()]
+
+def run_test(layers):
+
+    gc.prj.model.layers = layers
     m.get_model()
 
     m.evaluate_model_metrics()
@@ -18,5 +18,20 @@ for i in range(5):
     dat.gen_image_summary(3, 3)
 
     gc.deinit_test_data()
+
+
+for i in range(5):
+    gc.init_test_data(lr=0.01, optimizer='sgd', loss_func='categorical_crossentropy', activation='relu', callback=None,
+                      nb_epochs=2, batch_siz=8 * 1024)
+    dat.proc_data()
+    layers = [l.IpDense(512), l.DropOut((i+1)/20), l.Dense(512), l.Dropout(0.2), l.OpDense()]
+    run_test(layers)
+
+    gc.init_test_data(lr=0.01, optimizer='sgd', loss_func='categorical_crossentropy', activation='relu', callback=None,
+                      nb_epochs=2, batch_siz=8 * 1024)
+    dat.proc_data()
+    layers = [l.ConvIp(6, (3, 3)), l.AvgPooling(), l.Conv(16, (3, 3)), l.AvgPooling(), l.Flat(), l.Dense(120),
+              l.Dense(84), l.OpDense()]
+    run_test(layers)
 
 exit(0)
